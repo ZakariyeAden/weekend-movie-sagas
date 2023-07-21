e2559868487495cdc3fc2762d6ec7faeb0e9065a
ZakariyeAden <zakariye5417@gmail.com>
2023-07-21 16:09:20 -0500

    adding Card and Fixing the Grid

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,7 +2,12 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./MovieList.css";
 import Grid from "@mui/material/Grid";
-
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 function MovieList() {
   const dispatch = useDispatch();
   const movies = useSelector(store => store.movies);
@@ -19,11 +24,22 @@ function MovieList() {
         <Grid container spacing={2}>
           {movies.map(movie => {
             return (
-              <Grid item lg={2} md={4} xs={7}>
-                <div key={movie.id}>
-                  <img src={movie.poster} alt={movie.title} />
-                  <h3>{movie.title}</h3>
-                </div>
+              <Grid item lg={2} md={4} xs={6}>
+                <Card
+                  key={movie.id}
+                  sx={{ maxWidth: 345 }}
+                  md={{ maxWidth: 345 }}
+                  lg={{ maxWidth: 345 }}
+                >
+                  <CardMedia
+                    sx={{ height: 280 }}
+                    image={movie.poster}
+                    title={movie.title}
+                  />
+                </Card>
+                <Typography gutterBottom variant="h5" component="div">
+                  {movie.title}
+                </Typography>
               </Grid>
             );
           })}
