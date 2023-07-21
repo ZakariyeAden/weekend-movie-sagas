9f3b14cc921ed401b9f8f97e1abd64c5e3e274d5
ZakariyeAden <zakariye5417@gmail.com>
2023-07-21 15:55:54 -0500

    adding Grid and responsive on all devices

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,36 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./MovieList.css";
+import Grid from "@mui/material/Grid";
 
 function MovieList() {
+  const dispatch = useDispatch();
+  const movies = useSelector(store => store.movies);
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+  useEffect(() => {
+    dispatch({ type: "FETCH_MOVIES" });
+  }, []);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
-
-    );
+  return (
+    <main>
+      <h1>MovieList</h1>
+      <section className="movies">
+        {/* Making it responsive on all devices by using mui Grid */}
+        <Grid container spacing={2}>
+          {movies.map(movie => {
+            return (
+              <Grid item lg={2} md={4} xs={7}>
+                <div key={movie.id}>
+                  <img src={movie.poster} alt={movie.title} />
+                  <h3>{movie.title}</h3>
+                </div>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </section>
+    </main>
+  );
 }
 
 export default MovieList;
