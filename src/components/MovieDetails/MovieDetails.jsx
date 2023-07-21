900019156b4c6a6166f67b6334fa63e902f338b5
ZakariyeAden <zakariye5417@gmail.com>
2023-07-21 16:26:14 -0500

    It is getting the Id When clicked to details in the Route

---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MovieDetails = () => {
+   const movies = useSelector(store => store.movies);
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default MovieDetails
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,13 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./MovieList.css";
-import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import { Grid, CardMedia, Typography, Card } from "@mui/material";
+import { Link } from "react-router-dom";
 function MovieList() {
   const dispatch = useDispatch();
   const movies = useSelector(store => store.movies);
@@ -24,23 +19,25 @@ function MovieList() {
         <Grid container spacing={2}>
           {movies.map(movie => {
             return (
-              <Grid item lg={2} md={4} xs={6}>
-                <Card
-                  key={movie.id}
-                  sx={{ maxWidth: 345 }}
-                  md={{ maxWidth: 345 }}
-                  lg={{ maxWidth: 345 }}
-                >
-                  <CardMedia
-                    sx={{ height: 280 }}
-                    image={movie.poster}
-                    title={movie.title}
-                  />
-                </Card>
-                <Typography gutterBottom variant="h5" component="div">
-                  {movie.title}
-                </Typography>
-              </Grid>
+                <Grid item lg={2} md={4} xs={6}>
+                    <Link to={`/details${movie.id}`}>
+                  <Card
+                    key={movie.id}
+                    sx={{ maxWidth: 345 }}
+                    md={{ maxWidth: 345 }}
+                    lg={{ maxWidth: 345 }}
+                  >
+                    <CardMedia
+                      sx={{ height: 280 }}
+                      image={movie.poster}
+                      title={movie.title}
+                    />
+                  </Card>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {movie.title}
+                  </Typography>
+              </Link>
+                </Grid>
             );
           })}
         </Grid>
