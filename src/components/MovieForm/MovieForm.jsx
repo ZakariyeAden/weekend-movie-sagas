f730a744a56fb751343dd02dadf1ebc0702ba79d
ZakariyeAden <zakariye5417@gmail.com>
2023-07-22 15:45:45 -0500

    working on changes for form

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,15 +1,65 @@
-import React from 'react'
-import { TextField,Button} from "@mui/material";
-const MovieForm = () => {
-  return (
-    <form>
-      <h2>Add a Movie</h2>
-      <TextField id="outlined-basic" label="Movie title" variant="outlined" />
-      <TextField id="outlined-basic" label="Movie image" variant="outlined" />
-      <TextField id="outlined-basic" label="Movie description" variant="outlined" />
-      <Button variant="contained" >Add Movie</Button>
-    </form>
-  )
-}
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
-export default MovieForm
+import { TextField, Button } from "@mui/material";
+const MovieForm = () => {
+  // HOOKS
+  const [movieTitle, setMovieTitle] = useState("");
+  const [movieDescription, setMovieDescription] = useState("");
+  const [movieImage, setMovieImage] = useState("");
+  const handleTitleChange = event => {
+    console.log("added title!");
+    // Set the user input for title
+    setMovieTitle(event.target.value);
+  };
+  const handleDescriptionChange = event => {
+    console.log("added description!");
+    // Set the user input for description
+    setMovieDescription(event.target.value);
+  };
+  const handleImageChange = event => {
+    console.log("added image!");
+    // Set the user input for image
+    setMovieImage(event.target.value);
+  };
+
+  let movieData = {
+    title: movieTitle,
+    description: movieDescription,
+    poster: movieImage,
+  };
+  const submitMovieHandler = event => {
+    console.log("Movie submitted!", movieData);
+  };
+  return (
+    <form onSubmit={submitMovieHandler}>
+      <h2>Add a Movie</h2>
+      <TextField
+        id="outlined-basic"
+        label="Movie title"
+        variant="outlined"
+        type="text"
+        onChange={handleTitleChange}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Movie image"
+        variant="outlined"
+        type="text"
+        onChange={handleImageChange}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Movie description"
+        variant="outlined"
+        type="text"
+        onChange={handleDescriptionChange}
+      />
+      <Button variant="contained" type="submit">
+        Add Movie
+      </Button>
+    </form>
+  );
+};
+
+export default MovieForm;
--- src/index.js
+++ src/index.js
@@ -1,94 +1,110 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App.js';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App.js";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 // Provider allows us to use redux within our react app
-import { Provider } from 'react-redux';
-import logger from 'redux-logger';
+import { Provider } from "react-redux";
+import logger from "redux-logger";
 // Import saga middleware
-import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put } from 'redux-saga/effects';
-import axios from 'axios';
+import createSagaMiddleware from "redux-saga";
+import { takeEvery, put } from "redux-saga/effects";
+import axios from "axios";
 
 // Create the rootSaga generator function
 function* rootSaga() {
-    yield takeEvery('FETCH_MOVIES', fetchAllMovies);
-    yield takeEvery('FETCH_MOVIE_ITEM', fetchIndivualMovie);
+  yield takeEvery("FETCH_MOVIES", fetchAllMovies);
+  yield takeEvery("FETCH_MOVIE_ITEM", fetchIndivualMovie);
+  yield takeEvery("ADD_MOVIES", addMovies);
 }
 
 function* fetchAllMovies() {
-    // get all movies from the DB
-    try {
-        const movies = yield axios.get('/api/movie');
-        console.log('get all:', movies.data);
-        yield put({ type: 'SET_MOVIES', payload: movies.data });
-
-    } catch {
-        console.log('get all error');
-    }
-        
+  // get all movies from the DB
+  try {
+    const movies = yield axios.get("/api/movie");
+    console.log("get all:", movies.data);
+    yield put({ type: "SET_MOVIES", payload: movies.data });
+    // Catch Errors
+  } catch {
+    console.log("get all error");
+  }
 }
-// Details 
-function* fetchIndivualMovie(action){
-    try{
-        const movies = yield axios.get(`/api/movie/${action.payload}`);
-        yield put({ type: 'SET_MOVIE_ITEM', payload: movies.data });
-        console.log('Indiviual movie:',movies.data)
-    }catch(err){
-        console.log('ERRORS in getting indiviual movie',err);
-    }
+// Details/Indiviual movie page
+function* fetchIndivualMovie(action) {
+  try {
+    const movies = yield axios.get(`/api/movie/${action.payload}`);
+    yield put({ type: "SET_MOVIE_ITEM", payload: movies.data });
+    console.log("Indiviual movie:", movies.data);
+    // Catch Errors
+  } catch (err) {
+    console.log("ERRORS in getting indiviual movie", err);
+  }
+}
+// POST: post the data recieved in the DB and display it in the DOM!
+function* addMovies(action) {
+  try {
+    yield axios.post("/api/movie/", {
+      title: action.payload,
+      poster: action.payload,
+      description: action.payload,
+    });
+    // Update in the GET
+    yield put({ type: "FETCH_MOVIES" });
+    // Catch Errors
+  } catch (err) {
+    console.log("ERROR in posting Movies in addMovies:", err);
+  }
 }
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
 // Used to store movies returned from the server
 const movies = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_MOVIES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_MOVIES":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 // Used to store the movie genres
 const genres = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_GENRES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_GENRES":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 // Indiviual movie item
 const movieItem = (state = {}, action) => {
-    switch (action.type) {
-        case 'SET_MOVIE_ITEM':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "SET_MOVIE_ITEM":
+      return action.payload;
+    default:
+      return state;
+  }
+};
 // Create one store that all components can use
 const storeInstance = createStore(
-    combineReducers({
-        movies,
-        genres,
-        movieItem
-    }),
-    // Add sagaMiddleware to our store
-    applyMiddleware(sagaMiddleware, logger),
+  combineReducers({
+    movies,
+    genres,
+    movieItem,
+  }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={storeInstance}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
